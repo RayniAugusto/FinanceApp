02d7c4ffd35dcbff501680a88f1d387e4cabfdbc
Rayni Augusto <augustdrs@gmail.com>
2023-07-26 23:44:22 -0500

    decimales y descending e imagen

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -25,14 +25,9 @@ class BalanceController extends Controller
             return $moneyout;
         });
 
-        $balance = $moneyins->concat($moneyouts)->sortBy('created_at');
+        $balance = $moneyins->concat($moneyouts)->sortByDesc('created_at');
         $total = $balance->sum('amount');
 
-//            $balance = [
-//                ['date' => '2023-08-01', 'tipo' => 'Ingreso', 'description' => 'Salario', 'amount' => 5000],
-//                ['date' => '2023-08-02', 'tipo' => 'Egreso', 'description' => 'Alquiler', 'amount' => 1500],
-//                ['date' => '2023-08-03', 'tipo' => 'Egreso', 'description' => 'Comida', 'amount' => 2000],
-//            ];
         return view('balance.index', compact('balance','total'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        @auth
-                            {{ __('You are logged in!') }}
-                        @endauth
-                    </div>
+            @guest
+                <div class="col-4">
+                    <h1>Hola!</h1>
+                    <p>Gestiona tus finanzas de manera eficiente y efectiva.</p>
                 </div>
-            </div>
+                <div class="col-6">
+                    <img id="bg" class="img-fluid" src="{{ asset('images/fondo.jpg') }}">
+                </div>
+            @endguest
+            @auth
+                <div class="col-4">
+                    <h1>Bienvenido a la aplicación FinanceApp</h1>
+                    <p>Gestiona tus finanzas de manera eficiente y efectiva.</p>
+                </div>
+                <div class="col-6">
+                    <img id="bg" class="img-fluid" src="{{ asset('images/fondo.jpg') }}">
+                </div>
+            @endauth
+
         </div>
     </div>
+
 @endsection
